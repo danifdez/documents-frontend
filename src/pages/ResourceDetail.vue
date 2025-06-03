9c8fb673c3d8742036fa4634a10c5ce6604701e4
Daniel Fernández <daniel.fdez.fdez@gmail.com>
2025-06-03 11:11:08 +0200

    Download button without text

--- src/pages/ResourceDetail.vue
+++ src/pages/ResourceDetail.vue
@@ -58,12 +58,11 @@
                         <div>
                             <a @click="downloadResource"
                                 class="inline-flex items-center px-4 py-1 text-sm font-medium text-white bg-blue-600 rounded-lg hover:bg-blue-700 cursor-pointer">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2" fill="none"
-                                    viewBox="0 0 24 24" stroke="currentColor">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24"
+                                    stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                         d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
                                 </svg>
-                                Download
                             </a>
                         </div>
                         <div class="flex rounded-md shadow-sm" role="group">
