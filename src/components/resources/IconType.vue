745a109c32c4de7ae369f94ca60064fa2b1df183
Daniel Fernández <daniel.fdez.fdez@gmail.com>
2025-06-13 12:12:17 +0200

    feat: update image file icon SVG for improved rendering

--- src/components/resources/IconType.vue
+++ src/components/resources/IconType.vue
@@ -28,8 +28,11 @@
                 clip-rule="evenodd" />
         </svg>
 
-        <svg v-else-if="isImageFile" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-            <path fill-rule="evenodd" d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm0 2h12v10H4V5zm2 2a1 1 0 110 2 1 1 0 010-2zm8 6l-2.5-3-3.5 4.5h10l-4-5.5z" clip-rule="evenodd" />
+        <svg v-else-if="isImageFile" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20"
+            fill="currentColor">
+            <path fill-rule="evenodd"
+                d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm0 2h12v10H4V5zm2 2a1 1 0 110 2 1 1 0 010-2zm8 6l-2.5-3-3.5 4.5h10l-4-5.5z"
+                clip-rule="evenodd" />
         </svg>
 
         <svg v-else xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
