e7859c6d34d99438e0967d77a63ec7e0b130e7bb
Daniel Fernández <daniel.fdez.fdez@gmail.com>
2025-10-24 13:13:14 +0200

    feat: conditionally render translate button based on translated content availability

--- src/components/resources/Toolbar.vue
+++ src/components/resources/Toolbar.vue
@@ -14,7 +14,7 @@
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
                 </svg>
             </Button>
-            <Button @click="emit('translate')" size="small" title="Translate Resource">
+            <Button v-if="!hasTranslatedContent" @click="emit('translate')" size="small" title="Translate Resource">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24"
                     stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
